c5c90aa8d44b1869f021e9a5462c5b13957bf10e
Patuljak <patuljakiztuk@gmail.com>
2022-12-25 02:23:50 +0100

    Replace switch with if statements

--- src/main/java/tuk/patuljak/brainfuck/Brainfuck.java
+++ src/main/java/tuk/patuljak/brainfuck/Brainfuck.java
@@ -14,38 +14,45 @@ public class Brainfuck {
             while(instructionIndex < instructions.length) {
                 var currentInstruction = instructions[instructionIndex++];
 
-                switch (currentInstruction) {
-                    case 43 -> ++memory[ptr];
-                    case 45 -> --memory[ptr];
-                    case 62 -> ++ptr;
-                    case 60 -> --ptr;
-                    case 46 -> System.out.print((char) memory[ptr]);
-                    case 91 -> {
-                        if(memory[ptr] != 0) {
-                            loopStarts[++lastLoopStart] = instructionIndex;
-                        } else {
-                            for(openBrackets = 1;
-                                openBrackets > 0;
-                                openBrackets += (currentInstruction == 91)
-                                    ? 1
-                                    : (currentInstruction == 93)
-                                        ? -1
-                                        : 0
-                            ) {
-                                currentInstruction = instructions[++instructionIndex];
-                            }
+                if(currentInstruction == 43)
+                    ++memory[ptr];
 
-                            ++instructionIndex;
-                        }
+                if(currentInstruction == 45)
+                    --memory[ptr];
+
+                if(currentInstruction == 60)
+                    --ptr;
+
+                if(currentInstruction == 62)
+                    ++ptr;
+
+                if(currentInstruction == 46)
+                    System.out.print((char) memory[ptr]);
+
+                if(currentInstruction == 93)
+                    if(memory[ptr] != 0) {
+                        instructionIndex = loopStarts[lastLoopStart];
+                    } else {
+                        --lastLoopStart;
                     }
-                    case 93 -> {
-                        if(memory[ptr] != 0) {
-                            instructionIndex = loopStarts[lastLoopStart];
-                        } else {
-                            --lastLoopStart;
+
+                if(currentInstruction == 91)
+                    if(memory[ptr] != 0) {
+                        loopStarts[++lastLoopStart] = instructionIndex;
+                    } else {
+                        for(openBrackets = 1;
+                            openBrackets > 0;
+                            openBrackets += (currentInstruction == 91)
+                                ? 1
+                                : (currentInstruction == 93)
+                                ? -1
+                                : 0
+                        ) {
+                            currentInstruction = instructions[++instructionIndex];
                         }
+
+                        ++instructionIndex;
                     }
-                }
             }
         }
     }
