33dfdc245b47010363c78d6acbefbfe6189c8307
Patuljak <patuljakiztuk@gmail.com>
2022-12-25 02:32:28 +0100

    Remove nested loop-skipping loop

--- src/main/java/tuk/patuljak/brainfuck/Brainfuck.java
+++ src/main/java/tuk/patuljak/brainfuck/Brainfuck.java
@@ -4,7 +4,7 @@ public class Brainfuck {
     static void main(final String[] programs) {
         for(var program : programs) {
             int ptr = 0,
-                openBrackets,
+                skippedLoops = 0,
                 lastLoopStart = 0,
                 instructionIndex = 0;
             var memory = new int[100];
@@ -14,45 +14,42 @@ public class Brainfuck {
             while(instructionIndex < instructions.length) {
                 var currentInstruction = instructions[instructionIndex++];
 
-                if(currentInstruction == 43)
-                    ++memory[ptr];
+                if(skippedLoops > 0)
+                    skippedLoops += (currentInstruction == 91)
+                        ? 1
+                        : (currentInstruction == 93)
+                        ? -1
+                        : 0;
+                else {
+                    if(currentInstruction == 43)
+                        ++memory[ptr];
 
-                if(currentInstruction == 45)
-                    --memory[ptr];
+                    if(currentInstruction == 45)
+                        --memory[ptr];
 
-                if(currentInstruction == 60)
-                    --ptr;
+                    if(currentInstruction == 60)
+                        --ptr;
 
-                if(currentInstruction == 62)
-                    ++ptr;
+                    if(currentInstruction == 62)
+                        ++ptr;
 
-                if(currentInstruction == 46)
-                    System.out.print((char) memory[ptr]);
+                    if(currentInstruction == 46)
+                        System.out.print((char) memory[ptr]);
 
-                if(currentInstruction == 93)
-                    if(memory[ptr] != 0) {
-                        instructionIndex = loopStarts[lastLoopStart];
-                    } else {
-                        --lastLoopStart;
-                    }
-
-                if(currentInstruction == 91)
-                    if(memory[ptr] != 0) {
-                        loopStarts[++lastLoopStart] = instructionIndex;
-                    } else {
-                        for(openBrackets = 1;
-                            openBrackets > 0;
-                            openBrackets += (currentInstruction == 91)
-                                ? 1
-                                : (currentInstruction == 93)
-                                ? -1
-                                : 0
-                        ) {
-                            currentInstruction = instructions[++instructionIndex];
+                    if(currentInstruction == 93)
+                        if(memory[ptr] != 0) {
+                            instructionIndex = loopStarts[lastLoopStart];
+                        } else {
+                            --lastLoopStart;
                         }
 
-                        ++instructionIndex;
-                    }
+                    if(currentInstruction == 91)
+                        if(memory[ptr] != 0) {
+                            loopStarts[++lastLoopStart] = instructionIndex;
+                        } else {
+                            ++skippedLoops;
+                        }
+                }
             }
         }
     }
